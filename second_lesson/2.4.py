23ad8866a4722c064ffc4611c5f5651ecf19e384
an.mitrofanov <cj-alex90@yandex.ru>
2022-08-06 16:15:59 +0300

    third lesson

--- second_lesson/2.4.py
+++
@@ -1,6 +0,0 @@
-text = input("Введите несколько слов через пробел: ")
-
-for number, word in enumerate(text.split(), 1):
-    if len(word) > 10:
-        word = word[:10] + "..."
-    print(f"{number}: {word}")
